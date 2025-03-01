23a512d6e7cb667bf5d438a630a29b79d0813983
Jamie Giarratana <jamie.giarratana@gmail.com>
2025-03-01 01:15:37 -0500

    add my half of instruction functions

--- GritVM.cpp
+++ GritVM.cpp
@@ -1,8 +1,52 @@
+#include <fstream>
+#include <iostream>
+
 #include "GritVM.hpp"
+#include "GritVMBase.hpp" // Assuming STATUS is defined in GritVMBase.hpp
 
 // implement instructions below
 // dont forget to add their declaration to the header file
 
 void GritVM::ADDMEM(long &a, long X) {
+void GritVM::ADDMEM(long &a, long X)
+{
     a += dataMem[X];
+}
+
+void GritVM::SUBMEM(long &a, long X)
+{
+    a -= dataMem[X];
+}
+
+void GritVM::MULMEM(long &a, long X)
+{
+    a *= dataMem[X];
+}
+
+void GritVM::DIVMEM(long &a, long X)
+{
+    a /= dataMem[X];
+}
+
+void GritVM::JUMPREL(long jump)
+{
+    currInstruct++;
+}
+
+void GritVM::JUMPNZERO(long jump)
+{
+    if (accumulator != 0)
+    {
+        currInstruct++;
+    }
+}
+
+void GritVM::NOOP()
+{
+    // do nothing
+}
+
+void GritVM::HALT()
+{
+    gritStatus = STATUS::HALTED;
 }
--- GritVM.hpp
+++ GritVM.hpp
@@ -12,12 +12,25 @@ private:
     std::vector<long> dataMem;
     std::list<Instruction> instructMem;
     std::list<Instruction>::iterator currInstruct;
-    STATUS gritStatus;
     long accumulator;
+    GritVM();
+    ~GritVM();
+
 public:
+    STATUS gritStatus;
     // add instruction functions here
     
     void ADDMEM(long &a, long X);
+    void SUBMEM(long &a, long X);
+    void MULMEM(long &a, long X);
+    void DIVMEM(long &a, long X);
+    void JUMPREL(long jump);
+    void JUMPNZERO(long jump);
+    void NOOP();
+    void HALT();
+    void OUTPUT();
+    void CHECKMEM(long Z);
+    STATUS load(const std::string filename, const std::vector<long> &initialMemory) = 0;
 };
 
 #endif
